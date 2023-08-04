6ba175ecad7c6054ad74693078f89e34426d71b7
CooXieM <99296416+CooXieM@users.noreply.github.com>
2023-08-04 12:12:25 +1200

    Add files via upload

--- Final_Base.py
+++ Final_Base.py
@@ -18,10 +18,9 @@ def statement_generator(statement, decoration):
     return ""
 
 
-# Def yes_no for the instructions
-# Checkers user has answered yes / no to a question
-def yes_no(question):
-    to_check = ["yes", "no"]
+# Gets the users answers to instructions and their desired shape
+def choice_checker(question):
+    to_check = ["yes", "no", "square", "circle", "rectangle", "triangle", "xxx"]
 
     valid = False
     while not valid:
@@ -37,35 +36,7 @@ def yes_no(question):
         print("Please enter either Yes or No: \n")
 
 
-# Def Shape Checker and put a list
-def Shape_check(question):
-    to_check = ["square", "circle", "rectangle", "triangle", "xxx", "s", "r", "c", "t"]
-
-    while True:
-        response = input(question).lower()
-
-        for shape_chosen in to_check:
-            if response == shape_chosen:
-                return response
-
-
-# Ask the user if they want to find the area or perimeter
-def aop(question):
-    while True:
-        response = input(question).lower()
-
-        if response == "area":
-            print("You Chose Area\n")
-            return response
-        elif response == "perimeter":
-            print("You Chose Perimeter\n")
-            return response
-        elif response == "xxx":
-            return response
-        else:
-            print("Please enter Area or Perimeter")
-
-
+# This checks the user enters a number
 def num_check(question):
     while True:
 
@@ -96,7 +67,7 @@ print(heading)
 statement_generator("Shape Area / Perimeter Calculator", "=")
 
 # Ask if instructions are needed -
-want_instructions = yes_no("\n Would you like to see the Instructions to this program?")
+want_instructions = choice_checker("\n Would you like to see the Instructions to this program?")
 if want_instructions == 'yes':
     print("Input your desired Shape and the Dimensions\n"
           "The program will calculate the area and perimeter\n"
@@ -105,53 +76,6 @@ if want_instructions == 'yes':
 # Error message
 error = "Please Try Again"
 
-
-# THIS FUNCTION DOES ALL THE CALCULATIONS
-# IT HAS 2 SECTIONS DECIDED BY AREA OR PERIMETER
-# THE FUNCTION ASK FOR THE LENGTHS TO FIND THE AREA / PERIMETER
-while True:
-    # Ask what shape the user desires
-    shape_c1 = Shape_check("\nPlease Pick a Valid Shape... (Square, Triangle, Rectangle or Circle)\n"
-                           "Else type 'xxx' to quit.\n").lower()
-
-    # Gets Dimensions for the Square
-    if shape_c1 == "square":
-        side_length = num_check("Side Length: ")
-        perimeter = side_length * 4
-        area = side_length ** 2
-
-        # Gets Dimensions for the Triangle
-        # figure out how to ask the user if they want area and perimeter
-    elif shape_c1 == "triangle":
-        base = num_check("Side 1 Length: ")
-        side = num_check("Side 2 Length: ")
-        other_side = num_check("Other Side Length:")
-        perimeter = base + side + other_side
-        hp = perimeter / 2
-        herons_law = math.sqrt(hp * (hp - base) * (hp - side) * (hp - other_side))
-        area = herons_law
-
-    # Gets Dimensions for the Circle
-    elif shape_c1 == "circle":
-        radius = num_check("Radius: ")
-        # add pi
-        perimeter = math.pi * math.sqrt(radius)
-        area = 2 * math.pi * radius
-
-    # Gets Dimensions for the Rectangle
-    elif shape_c1 == "rectangle":
-        base = num_check("Base Length: ")
-        length = num_check("Height Length: ")
-        perimeter = base * 2 + length * 2
-        area = base * length
-
-    elif shape_c1 == "xxx":
-        break
-    else:
-        print(error)
-
-
-# Add Panda's and then you're done
 # Dictionary - - - - - - - - - - - - -
 shapes = []
 area_panda = []
@@ -163,10 +87,59 @@ calculator_dict = {
     "Perimeter:": perimeter_panda
 }
 
-# Pandas ----
-shapes.append(shape_c1)
-area_panda.append(area)
-perimeter_panda.append(perimeter)
+# THIS FUNCTION DOES ALL THE CALCULATIONS
+# IT HAS 2 SECTIONS DECIDED BY AREA OR PERIMETER
+# THE FUNCTION ASK FOR THE LENGTHS TO FIND THE AREA / PERIMETER
+while True:
+    # Ask what shape the user desires
+    shape_c1 = choice_checker("\nPlease Pick a Valid Shape... (Square, Triangle, Rectangle or Circle)\n"
+                              "Else type 'xxx' to quit.\n").lower()
+
+    # Gets Dimensions for the Square
+    if shape_c1 == "square" or shape_c1 == "s":
+        side_length = num_check("Side Length: ")
+        perimeter = side_length * 4
+        area = side_length ** 2
+        print("Perimeter is {:.2f}. Area is {:.2f}".format(area, perimeter))
+
+        # Gets Dimensions for the Triangle
+        # figure out how to ask the user if they want area and perimeter
+    elif shape_c1 == "triangle":
+        base = num_check("Side 1 Length: ")
+        side = num_check("Side 2 Length: ")
+        other_side = num_check("Side 3 Length: ")
+        perimeter = base + side + other_side
+        hp = perimeter / 2
+        herons_law = math.sqrt(hp * (hp - base) * (hp - side) * (hp - other_side))
+        area = herons_law
+        print("Perimeter is {:.2f}. Area is {:.2f}".format(area, perimeter))
+
+    # Gets Dimensions for the Circle
+    elif shape_c1 == "circle":
+        radius = num_check("Radius: ")
+        # add pi
+        perimeter = math.pi * math.sqrt(radius)
+        area = 2 * math.pi * radius
+        print("Perimeter is {:.2f}. Area is {:.2f}".format(area, perimeter))
+
+    # Gets Dimensions for the Rectangle
+    elif shape_c1 == "rectangle":
+        base = num_check("Base Length: ")
+        length = num_check("Height Length: ")
+        perimeter = base * 2 + length * 2
+        area = base * length
+        print("Perimeter is {:.2f}. Area is {:.2f}".format(area, perimeter))
+
+    elif shape_c1 == "xxx":
+        break
+    else:
+        print(error)
+
+    # Pandas ----
+    shapes.append(shape_c1)
+    area_panda.append(area)
+    perimeter_panda.append(perimeter)
+# Add Panda's and then you're done
 
 calculator_frame = pandas.DataFrame(calculator_dict)
 
---
+++ test.py
@@ -0,0 +1,4 @@
+print("Input your desired Shape and the Dimensions)"
+print("The program will calculate the area and perimeter")
+print("Type 'xxx' to end the program)")
+
